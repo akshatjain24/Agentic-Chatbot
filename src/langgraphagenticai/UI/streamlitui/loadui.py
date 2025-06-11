bcd7658c53bfb07d1b2ea0be47a03714831259c4
akshatjain24 <akshatjainmail@gmail.com>
2025-06-12 01:02:44 +0530

    Added Chatbot with Websearch

--- src/langgraphagenticai/UI/streamlitui/loadui.py
+++ src/langgraphagenticai/UI/streamlitui/loadui.py
@@ -32,4 +32,9 @@ class LoadStreamlitUI:
             # Usecase Selection
             self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
 
+            if self.user_controls["selected_usecase"] == "Chatbot with Web":
+                os.environ["TAVILY_API_KEY"] = self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY", type="password")
+                # Validate API Key
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("⚠️ Please enter the TAVILY API key to proceed. Don't have? refer: https://app.tavily.com/home")
         return self.user_controls
